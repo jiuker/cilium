0407ba505772b62a310ebc19022cbcd3ef94dba8
Chris Tarazi <chris@isovalent.com>
2022-12-13 10:26:48 -0800

    test/helpers: Disambiguate executor logs
    [ upstream commit 527f5fd59356387d9a3f7136d0c21904fd275038 ]
    
    This makes it much easier to track down which code path the executor
    logs are coming from, so that when debugging issues, we can focus on the
    relevant code path.
    
    [ Backport note: Fixed minor conflict. ]
    
    Signed-off-by: Chris Tarazi <chris@isovalent.com>
    Signed-off-by: Quentin Monnet <quentin@isovalent.com>

--- test/helpers/local_node.go
+++ test/helpers/local_node.go
@@ -173,7 +173,7 @@ func (s *LocalExecutor) ExecContext(ctx context.Context, cmd string, options ...
 		ops = options[0]
 	}
 
-	log.Debugf("running command: %s", cmd)
+	log.Debugf("running local command: %s", cmd)
 	fmt.Fprintln(SSHMetaLogs, cmd)
 	stdout := new(Buffer)
 	stderr := new(Buffer)
@@ -195,7 +195,7 @@ func (s *LocalExecutor) ExecContext(ctx context.Context, cmd string, options ...
 		}
 		res.success = false
 
-		log.WithError(err).Errorf("Error executing command '%s'", cmd)
+		log.WithError(err).Errorf("Error executing local command '%s'", cmd)
 		res.err = err
 	}
 
@@ -244,7 +244,7 @@ func (s *LocalExecutor) ExecInBackground(ctx context.Context, cmd string, option
 			}
 			res.success = false
 
-			log.WithError(err).Errorf("Error executing command '%s'", strings.Join(append([]string{cmd.Path}, cmd.Args...), " "))
+			log.WithError(err).Errorf("Error executing local background command '%s'", strings.Join(append([]string{cmd.Path}, cmd.Args...), " "))
 			res.err = err
 		}
 
